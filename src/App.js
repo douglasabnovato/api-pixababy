ef5e66171cc7775130152df2e40bf4893b1fad1e
@douglasabnovato <douglasabnovato@gmail.com>
2019-12-27 16:11:10 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
+class MyButton extends Component{
+  render(){
+    return <button>Click Me</button>
+  }
+}
+
+class MyLabel extends Component{
+  render(){
+    return <p>Hello World from component.</p>
+  }
+}
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <p>hello world !</p>
+        <MyLabel />
+        <MyButton />
       </div>
     );
   }
