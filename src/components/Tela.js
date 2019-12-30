d69a77e70424139e32d6623e43aa99b1fbb00df3
@douglasabnovato <douglasabnovato@gmail.com>
2019-12-30 12:49:48 -0300

    componentes e props

--- src/components/Tela.js
+++ src/components/Tela.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class MyLabel extends Component{
     render(){
         return ( 
-            <p> Cliquei no: {this.props.text} </p>
+            <p> Click in: {this.props.text} </p>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import Tela from './components/Tela';
 import App from './components/App';
 import Formulario from './components/Formulario';
 import Welcome from './components/Welcome';
+import Comment from './components/Comment';
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+};
 
 ReactDOM.render(
     <div>
@@ -12,6 +22,11 @@ ReactDOM.render(
         <Tela />
         <App />
         <Formulario />
+        <Comment
+            date={comment.date}
+            text={comment.text}
+            author={comment.author}
+        />
     </div>,     
     document.getElementById('root')
 ); 
