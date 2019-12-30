1683f8175b86ecaca5ec7d146ff47984d65495b2
@douglasabnovato <douglasabnovato@gmail.com>
2019-12-30 17:45:05 -0300

    SearchBar-props and state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
 
 class App extends Component{
 
   render(){
     return(
       <div className="App">
-        <h2>testing App</h2>
+        <SearchBar/>
       </div>
     );
   }
